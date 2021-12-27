2da3e595bbb0d92ca8f3a46e24fc6b0d78ca055b
Justin Mello <mello23@llnl.gov>
2021-12-26 18:12:54 -0800

    Adding selection sort.

--- selection-sort.cpp
+++ selection-sort.cpp
@@ -2,17 +2,44 @@
 using namespace std;
 
 
+/* Swap Array */
+void swapItem(int *item1, int *item2) {
+    int temp = *item1;
+    *item1 = *item2;
+    *item2 = temp;
+}
+
+/* Selection Sort */
+void selectionSort(int array[], int arraySize) {
+    int min_index;
+
+    for(int i = 0; i < arraySize - 1; i++) {
+        min_index = i;
+        for(int j =  i+1; j < arraySize; j++) {
+            if(array[j] < array[min_index])
+                min_index = j;
+
+            swapItem(&array[min_index], &array[i]);
+        }
+    }
+}
+
+
+/* Print Array */
 void printArray(int arr[], int size) {
-    
     for(int i = 0; i < size; i++)
         cout << arr[i] << " ";
 }
+
+/* Main */
 int main() {
 
-    int array1[] = {10, 20, 30};
-    int arraySize = sizeof(array1)/sizeof(array1[0]);
+    int array[] = {30, 20, 10};
+    int arraySize = sizeof(array)/sizeof(array[0]);
     
-    printArray(array1, arraySize);
+    selectionSort(array, arraySize);
+    printArray(array, arraySize);
+    cout << "" << endl;
 
     return 0;
 }
